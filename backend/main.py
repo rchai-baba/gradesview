a5f9acf19bb171c43eca9679c820440124a836ff
Raymond Chai <chaichai28@midlandps.org>
2026-04-10 16:02:11 -0400

    uses /api

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app = FastAPI(title="GradesView API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:8080", "http://localhost:5173"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
